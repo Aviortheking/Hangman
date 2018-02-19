9aba2c63fe00436224c5d0c0e84f161e6d16fa26
Aviortheking <2bazert@gmail.com>
2018-02-19 15:20:38 +0100

    Yolo

--- app/src/main/java/net/DeltaWings/Android/Hangman/MainActivity.java
+++ app/src/main/java/net/DeltaWings/Android/Hangman/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import net.DeltaWings.Android.Hangman.settings.SettingActivity;
